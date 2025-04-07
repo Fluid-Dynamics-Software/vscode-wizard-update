24e547e5b6320c6ec2711ca5295239adff2aaa5c
hishamfangs <hishamfangs@gmail.com>
2025-04-07 11:53:38 +0200

    Added ability to add custom stylesheets
    By just adding at the extension root folder:
    wizardtemplates/<webview id>/style.css

--- src/pageImpl.ts
+++ src/pageImpl.ts
@@ -50,9 +50,10 @@ export function sendInitialData(wizardName: string, data: Map<string, string>) {
     const response: CommandResponse = {
       command: `InitializeData`,
     };
-    let obj = Array.from(data).reduce((obj, [key, value]) => (
-      Object.assign(obj, { [key]: value }) // Be careful! Maps can have non-String keys; object literals can't.
-    ), {});
+    let obj = Array.from(data).reduce(
+      (obj, [key, value]) => Object.assign(obj, { [key]: value }), // Be careful! Maps can have non-String keys; object literals can't.
+      {},
+    );
     response.result = obj;
     panel.webview.postMessage(response);
   }
@@ -71,18 +72,15 @@ export function createOrShowWizard(
   viewType: string,
   title: string,
   context: vscode.ExtensionContext,
-  messageMappings: MessageMapping[]) {
-
-  const pages: string = path.join(__dirname, "../", "pages").normalize();
-  const html: string = path.join(pages, "stub.html");
+  messageMappings: MessageMapping[],
+) {
+  const pages: string = path.join(__dirname, '../', 'pages').normalize();
+  const html: string = path.join(pages, 'stub.html');
 
   createOrShowWizardWithPaths(id, viewType, title, context, messageMappings, pages, html);
 }
 
-export function updatePanelTitle(
-  name: string,
-  title: string
-) {
+export function updatePanelTitle(name: string, title: string) {
   let panel = currentPanels.get(name);
   if (panel) {
     panel.title = title;
@@ -96,58 +94,46 @@ export function createOrShowWizardWithPaths(
   context: vscode.ExtensionContext,
   messageMappings: MessageMapping[],
   rootPath: string,
-  pagePath: string
+  pagePath: string,
 ) {
-  const rootPathUri : vscode.Uri = vscode.Uri.file(rootPath);
+  const rootPathUri: vscode.Uri = vscode.Uri.file(rootPath);
   let panel = currentPanels.get(id);
   if (panel) {
     panel.reveal();
   } else {
-    panel = vscode.window.createWebviewPanel(
-      viewType,
-      title,
-      vscode.ViewColumn.One,
-      {
-        enableScripts: true,
-        retainContextWhenHidden: true,
-        localResourceRoots: [
-          context.extensionUri,
-          rootPathUri
-        ],
-      }
-    );
-    const pagesWebviewUri : vscode.Uri = panel.webview.asWebviewUri(rootPathUri);
+    panel = vscode.window.createWebviewPanel(viewType, title, vscode.ViewColumn.One, {
+      enableScripts: true,
+      retainContextWhenHidden: true,
+      localResourceRoots: [context.extensionUri, rootPathUri],
+    });
+    const pagesWebviewUri: vscode.Uri = panel.webview.asWebviewUri(rootPathUri);
     const contents: string = fs.readFileSync(pagePath, 'utf-8');
-    const contentsReplaced = contents.split("{{base}}").join(pagesWebviewUri.toString());
-    panel.webview.html = contentsReplaced;
-    panel.webview.onDidReceiveMessage(
-      createDispatch(messageMappings, id, rootPath)
-    );
-    panel.onDidDispose(
-      () => currentPanels.delete(id),
-      undefined,
-      context.subscriptions
-    );
+    const contentsReplaced = contents.split('{{base}}').join(pagesWebviewUri.toString());
+
+    // Add a custom CSS file to the webview by webview id
+    // Located at the extension root folder under wizardtemplates/<webview id>/style.css
+    const extURI = context.extensionUri;
+    const extensionUri: vscode.Uri = panel.webview.asWebviewUri(extURI);
+    const extensionUriString = extensionUri.toString() + '/wizardtemplates/' + id + '/style.css';
+    const contentsWithCSS = contentsReplaced.split('{{style}}').join(extensionUriString.toString());
+
+    panel.webview.html = contentsWithCSS;
+    panel.webview.onDidReceiveMessage(createDispatch(messageMappings, id, rootPath));
+    panel.onDidDispose(() => currentPanels.delete(id), undefined, context.subscriptions);
     currentPanels.set(id, panel);
   }
 }
 
-function createDispatch(
-  messageMappings: MessageMapping[],
-  currentPanelName: string,
-  resourceRoot: string
-) {
+function createDispatch(messageMappings: MessageMapping[], currentPanelName: string, resourceRoot: string) {
   const handler = (message: any) => {
-    const mapping = messageMappings.find(
-      mapping => mapping.command === message.command
-    );
+    const mapping = messageMappings.find((mapping) => mapping.command === message.command);
     if (mapping) {
       const callback: AsyncMessageCallback = async (result: HandlerResponse): Promise<void> => {
         if (!result) {
           return;
         }
-        const forward: string | undefined = (result.forward === null ? mapping.defaultForward : result.forward);
-        if( forward ) {
+        const forward: string | undefined = result.forward === null ? mapping.defaultForward : result.forward;
+        if (forward) {
           return handler.call(null, {
             command: forward,
             parameters: result,
@@ -158,43 +144,41 @@ function createDispatch(
       };
       mapping.asyncHandler.call(null, callback, message.parameters);
     } else {
-      vscode.window.showErrorMessage(
-        `Can not find a handler for ${message.command}.`
-      );
+      vscode.window.showErrorMessage(`Can not find a handler for ${message.command}.`);
     }
   };
   return handler;
 }
 
-const postMessageHandlerResponse = async (mapping: MessageMapping, 
+const postMessageHandlerResponse = async (
+  mapping: MessageMapping,
   result: HandlerResponse,
   currentPanelName: string,
-  resourceRoot: string
-) => { 
+  resourceRoot: string,
+) => {
   const resp: CommandResponse = generatePostResponseFromHandlerResponse(mapping, result, resourceRoot);
   sendMessageToWebview(resp, currentPanelName);
-}
+};
 
 const sendMessageToWebview = (resp: CommandResponse, currentPanelName: string): void => {
   const panel: vscode.WebviewPanel | undefined = currentPanels.get(currentPanelName);
   if (panel && panel !== undefined) {
     panel.webview.postMessage(resp);
   }
-}
+};
 
 const generatePostResponseFromHandlerResponse = (
-  mapping: MessageMapping, 
+  mapping: MessageMapping,
   result: HandlerResponse,
-  resourceRoot: string
+  resourceRoot: string,
 ): CommandResponse => {
-
   const response: CommandResponse = {
     command: `${mapping.command}Response`,
   };
-  const templates: Template[] | undefined = (result.templates === null ? mapping.defaultTemplates : result.templates);
+  const templates: Template[] | undefined = result.templates === null ? mapping.defaultTemplates : result.templates;
   if (templates) {
     response.contents = [];
-    templates.forEach(template => {
+    templates.forEach((template) => {
       if (template.content) {
         response.contents?.push({
           id: template.id,
@@ -203,13 +187,7 @@ const generatePostResponseFromHandlerResponse = (
       } else if (template.contentUrl) {
         response.contents?.push({
           id: template.id,
-          body: handlebars.compile(
-            fs
-              .readFileSync(
-                path.join(resourceRoot, template.contentUrl)
-              )
-              .toString()
-          )(result),
+          body: handlebars.compile(fs.readFileSync(path.join(resourceRoot, template.contentUrl)).toString())(result),
         });
       }
     });
@@ -218,4 +196,4 @@ const generatePostResponseFromHandlerResponse = (
   }
   response.focusedField = result?.returnObject?.focusedField;
   return response;
-}
+};
